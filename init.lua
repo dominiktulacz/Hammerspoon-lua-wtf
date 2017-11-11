422230c24a881b5a8ce1633c8eaae67714d6c2cc
Dominik Tułacz <19214086+dominiktulacz@users.noreply.github.com>
2017-11-11 20:53:10 +0100

    Another refactor

--- init.lua
+++ init.lua
@@ -1,27 +1,9 @@
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "Right", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.y = max.y
-  f.x = max.x + max.w / 2
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+  moveFocusedWindowToRightSide()
 end)
 
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "Left", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.y = max.y
-  f.x = max.x
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+  moveFocusedWindowToLeftSide()
 end)
 
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "Up", function()
@@ -37,6 +19,87 @@ hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "Up", function()
   win:setFrame(f)
 end)
 
+function isWindowOnLeftSide(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  y = max.y
+  x = max.x
+  w = max.w / 2
+  h = max.h
+
+  if (x == f.x and y == f.y and w == f.w and h == f.h) then
+    return true
+  else
+    return false
+  end
+end
+
+function isWindowOnRightSide(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  y = max.y
+  x = max.x + max.w / 2
+  w = max.w / 2
+  h = max.h
+
+  if (x == f.x and y == f.y and w == f.w and h == f.h) then
+    return true
+  else
+    return false
+  end
+end
+
+function moveFocusedWindowToLeftSide()
+  local win = hs.window.focusedWindow()
+  if (isWindowOnLeftSide(win)) then
+    --todo switch screen
+  else
+    moveWindowToLeftSide(win)
+  end
+end
+
+function moveWindowToLeftSide(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  
+  f.y = max.y
+  f.x = max.x
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end
+
+function moveFocusedWindowToRightSide()
+  local win = hs.window.focusedWindow()
+  if (isWindowOnRightSide(win)) then
+    --todo switch screen
+  else
+    moveWindowToRightSide(win)
+  end
+end
+
+function moveWindowToRightSide(win)
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.y = max.y
+  f.x = max.x + max.w / 2
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end
+
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
 
 appArray = {
   {"1", "Google Chrome"},
@@ -52,15 +115,17 @@ appArray = {
   {"S", "Spotify"}
 }
 
+appArrayLength = tablelength(appArray)
+
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "H", function()
-  local msg = "Shortcuts (use it with 'caps lock'):"
-  for i = 1, 11 do
+  local msg = "Shortcuts [" .. appArrayLength .."] (use it with 'caps lock'):"
+  for i = 1, appArrayLength do
     msg = msg .. "\n" .. appArray[i][1] .. ". " .. appArray[i][2]
   end
   hs.alert.show(msg)
 end)
 
-for i = 1, 11 do
+for i = 1, appArrayLength do
   hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, appArray[i][1], function()
     hs.application.launchOrFocus(appArray[i][2])
   end)
